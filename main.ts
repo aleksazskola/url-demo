435861642e6c87c4cd021805e5fc0f480d48d8cb
aleksazskola <93650537+aleksazskola@users.noreply.github.com>
2021-11-05 15:19:40 +0100

    Ажурирање main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,15 +1,23 @@
 input.onButtonPressed(Button.A, function () {
     url = "" + url + "A"
 })
-input.onButtonPressed(Button.AB, function () {
-    radio.sendString(url)
+radio.onReceivedString(function (receivedString) {
+    if (receivedString == "mystery") {
+        music.playMelody("E B C5 A B G A F ", 120)
+    } else if (receivedString == "paris") {
+        music.playMelody("G F G A - F E D ", 120)
+    }
 })
 input.onButtonPressed(Button.B, function () {
     url = "" + url + "B"
 })
+input.onButtonPressed(Button.AB, function () {
+    radio.setGroup(100)
+    radio.sendString(url)
+})
 let url = ""
 url = ""
-radio.setGroup(101)
 basic.forever(function () {
-	
+    radio.setGroup(101)
+    scrollbit.scrollText(url, 128, 50)
 })
