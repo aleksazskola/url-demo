4340f4d603f27cd7e546a899423f9b677e85ffb4
aleksazskola <93650537+aleksazskola@users.noreply.github.com>
2021-11-05 14:57:19 +0100

    Update pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,15 @@
+input.onButtonPressed(Button.A, function () {
+    url = "" + url + "A"
+})
+input.onButtonPressed(Button.AB, function () {
+    radio.sendString(url)
+})
+input.onButtonPressed(Button.B, function () {
+    url = "" + url + "B"
+})
+let url = ""
+url = ""
+radio.setGroup(101)
 basic.forever(function () {
 	
 })
